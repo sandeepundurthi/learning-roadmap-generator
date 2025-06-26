ee5b6a27c5363f5fba99126febbf0af3d9402b4f
Sandeep Undurthi <165824173+sandeepundurthi@users.noreply.github.com>
2025-06-25 18:03:47 -0600

    Add files via upload

---
+++ backend/app/main.py
@@ -0,0 +1,24 @@
+from fastapi.middleware.cors import CORSMiddleware
+
+from fastapi import FastAPI
+from app.routes import roadmap, auth
+
+app = FastAPI(
+    title="Learning Roadmap Generator",
+    description="API to generate learning paths based on user skills and goals.",
+    version="1.0.0"
+)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3001"],  # or use ["*"] for dev
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+app.include_router(auth.router, prefix="/auth")
+app.include_router(roadmap.router, prefix="/roadmap")
+
+@app.get("/")
+def root():
+    return {"message": "Welcome to the Learning Roadmap Generator API"}
