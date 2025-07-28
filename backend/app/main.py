93a43d44bcad225023d52ce4ae16e23984269db0
Sandeep Undurthi <165824173+sandeepundurthi@users.noreply.github.com>
2025-07-28 14:16:29 -0600

    Update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -10,7 +10,7 @@ app = FastAPI(
 )
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3001"],  # or use ["*"] for dev
+    allow_origins=["http://localhost:3000"],  # or use ["*"] for dev
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
